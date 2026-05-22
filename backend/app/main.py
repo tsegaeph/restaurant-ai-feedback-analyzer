30823e16a7928fcd1f8868e13e84c95eb93760ad
tsegaeph <tsegaephrem080@gmail.com>
2026-05-22 21:55:44 +0300

    allowed CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://restaurant-ai-feedback-analyzer.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
